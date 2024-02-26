041e7d709446d282b1bc22fe2ea267a60b221147
kakkarav <kakkarav@berkeley.edu>
2024-02-26 19:15:40 +0700

    rename directories and fix import for pytest

--- tests/test_decompose.py
+++ tests/test_decompose.py
@@ -1,7 +1,12 @@
-from ..module.decomposition import decompose
 import numpy as np
-
 import pytest
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+import decomposition as decom
 
 matrix = np.eye(2)
 
@@ -9,4 +14,4 @@ testmatrix = np.kron(matrix, matrix)
 
 
 def test_decompose_indenity():
-    assert decompose(testmatrix)[(0, 0)] == 1
+    assert decom.decompose(testmatrix)[(0, 0)] == 1
