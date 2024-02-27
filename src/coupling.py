0f54c2557df3e53c0e9a5c267594e63556eb36d7
Kamphol Akkaravarawong <best@Kamphols-MacBook-Pro.local>
2024-02-27 14:10:23 +0700

    Add requirement file and use pytest.fixture for the tests

--- src/coupling.py
+++ src/coupling.py
@@ -3,11 +3,9 @@ from . import matrixElement as me
 
 
 class Coupling:
-    def __init__(
-        self,
-    ):
-        self.lat = lattice.Lattice(1, 1, 1, 1)
-        self.shiba = params.Shiba(1, 1, 1, 1, 1)
+    def __init__(self, vec1, vec2, N, R, alpha, beta, lambdaF, xi, B):
+        self.lat = lattice.Lattice(vec1, vec2, N, R)
+        self.shiba = params.Shiba(alpha, beta, lambdaF, xi, B)
 
     def coupling(self):
         return me.U
--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -8,13 +8,26 @@ from src.coupling import Coupling
 
 
 @pytest.fixture
-def shiba():
-    return Coupling()
+def param():
+    vec1 = np.array([1, 0])
+    vec2 = np.array([0, 1])
+    N = 10
+    R = 1
+    alpha = 1
+    beta = 1
+    lambdaF = 1
+    xi = 1
+    B = 1
+    return [vec1, vec2, N, R, alpha, beta, lambdaF, xi, B]
 
 
-def test_location():
-    shiba().shiba.xi
-    pass
+@pytest.fixture
+def Shib(param):
+    return Coupling(*param)
+
+
+def test_location(Shib):
+    assert Shib.shiba.xi == 1
 
 
 def test_distanc():
