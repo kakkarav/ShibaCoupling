Correct the coords order for the thirdOrder calculation

main.py
@@ -2,31 +2,54 @@ import numpy as np
 from src.coupling import Coupling
 
 
+# Example of how to use the the code
+#
 def params():
+    # The lattice unit vectors in the Cartesian coordinate
     vec1 = np.array([1, 0])
     vec2 = np.array([0, 1])
-    N = 10
+    # The lattice spacing in meter
     R = 1
+    # The dimensionless exchange coupling
     alpha = 0.9
+    # The small perturbative parameter for the spin flip term
     beta = 1
+    # Fermi wavelength in meter
     lambdaF = 1
+    # Superconductor coherence length in meter
     xi = 1
+    # Magnetic field in Tesla
     B = 1
-    return [vec1, vec2, N, R, alpha, beta, lambdaF, xi, B]
+    return [vec1, vec2, R, alpha, beta, lambdaF, xi, B]
 
 
 if __name__ == "__main__":
+    # Inititate the coupling class
     shib = Coupling(*params())
-    # print(shib.secondOrder([0, 1], [0, 0]))
-    # print(shib.thirdOrder([0, 1], [0, 0], [1, 0]))
+
+    # Compute the effecitve coupling for the second order perturbation
+    # between impurities at coordinate (1,0) and (0,0)
     second = shib.secondOrder([0, 1], [0, 0])
 
+    # All of the paramters are stored in
+    print("Here are the model paramters")
+    print(shib.shiba)
+
+    # You can find the actualy distance between two points
+    # at coord1 and coord2
+    print(f"Distance between two point : {shib.lat.distance([1,0], [0,0])}\n")
+
+    # Compute the effecitve coupling for the third order perturbation
+    # between impurities at coordinate (0,1), (0, 0), and (1,0)
     third = shib.thirdOrder([0, 1], [0, 0], [1, 0])
+
+    # the result is store in the dictionary where the key is the tuple of pauli string
+    # E.g. (0,0,1) = IIX, (1,2,3) = XYZ
     print("Second order perturbation")
     for key, value in second.items():
         if value != 0.0:
             print(key, value)
-    print("=============================================")
+    print("=============================================\n")
     print("Third order perturbation")
     for key, value in third.items():
         if value != 0.0:

src/classes/lattice.py
@@ -2,17 +2,16 @@ import numpy as np
 
 
 class Lattice:
-    def __init__(self, vec1: np.ndarray, vec2: np.ndarray, N: int, R) -> None:
+    def __init__(self, vec1: np.ndarray, vec2: np.ndarray, R: float) -> None:
         self.vec1 = np.array(vec1)
         self.vec2 = np.array(vec2)
-        self.size = N
         self.spacing = R
 
     def location(self, coord: np.ndarray) -> np.ndarray:
         """
         Calculate the absolute coordinate in distance
         """
-        return coord[0] * self.vec1 + coord[1] * self.vec2
+        return (coord[0] * self.vec1 + coord[1] * self.vec2) * self.spacing
 
     def distance(self, coord1, coord2):
         """
@@ -30,7 +29,8 @@ class Lattice:
         """
         x1, y1 = self.location(coord1)
         x2, y2 = self.location(coord2)
-        return B * (x1 * y2 - x2 * y1) / 2
+        normalization = 1
+        return normalization * B * (x1 * y2 - x2 * y1) / 2
 
     def area(self, coord1, coord2, coord3):
         """
@@ -40,4 +40,4 @@ class Lattice:
         x1, y1 = coord1
         x2, y2 = coord2
         x3, y3 = coord3
-        return (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
+        return self.spacing**2 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2

tests/test_lattice.py
@@ -8,14 +8,13 @@ from src.coupling import Coupling
 def param():
     vec1 = np.array([1, 0])
     vec2 = np.array([0, 1])
-    N = 10
     R = 1
     alpha = 0.9
     beta = 1
     lambdaF = 1
     xi = 1
     B = 1
-    return [vec1, vec2, N, R, alpha, beta, lambdaF, xi, B]
+    return [vec1, vec2, R, alpha, beta, lambdaF, xi, B]
 
 
 @pytest.fixture