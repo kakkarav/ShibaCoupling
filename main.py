Fix the phase in the third-order calculation
There is numerical instatbility which generate undesirable terms such as (1,1,3)

main.py
@@ -21,9 +21,9 @@ def params():
     # Fermi wavelength in meter
     lambdaF = 0.001
     # Superconductor coherence length in meter
-    xi = 1000
+    xi = 10
     # Magnetic field in Tesla
-    B = 100
+    B = 0.1
     return [vec1, vec2, R, alpha, beta, lambdaF, xi, B]
 
 
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
     # Compute the effecitve coupling for the second order perturbation
     # between impurities at coordinate (1,0) and (0,0)
-    second = shib.secondOrder([0, 1], [0, 0])
+    second = shib.secondOrder([0, 1], [0, 2])
 
     # All of the paramters are stored in
     print("Here are the model paramters")
@@ -47,26 +47,30 @@ if __name__ == "__main__":
 
     # Compute the effecitve coupling for the third order perturbation
     # between impurities at coordinate (0,1), (0, 0), and (1,0)
-    third = shib.thirdOrder([0, 1], [0, 0], [1, 0])
+    third = shib.thirdOrder([0, 4], [0, 5], [0, 6])
 
     # the result is store in the dictionary where the key is the tuple of pauli string
     # E.g. (0,0,1) = IIX, (1,2,3) = XYZ
+
+    # Print out the result
     print("Second order perturbation")
     for key, value in second.items():
         if value != 0.0:
             print(key, value)
+
     print("=============================================\n")
     print("Third order perturbation")
     for key, value in third.items():
-        if np.abs(value) != 0:
-            print(key, value)
-        # print(key, value)
+        if np.abs(value) > 1e-14:
+            print(f"{key} : {np.real(value)}")
+        else:
+            print(f"\033[93m {key}: {np.real(value)}\033[0m")
 
     print("=============================================\n")
     print(me.A(shib.shiba, 0, np.sqrt(2) / 2 / np.pi))
-    print(me.B(shib.shiba, 0, np.sqrt(2) / 2 / np.pi))
-    print(me.C(shib.shiba, 0, np.sqrt(2) / 2 / np.pi))
-    print(me.D(shib.shiba, 0, np.sqrt(2) / 2 / np.pi))
-    print(me.cc(me.D(shib.shiba, 0, np.sqrt(20 / 2 / np.pi))))
-
-    print(np.pi / 4 / shib.lat.phase(shib.shiba.B, np.array([1, 0]), np.array([0, 1])))
+    # print(me.B(shib.shiba, 0, np.sqrt(2) / 2 / np.pi))
+    # print(me.C(shib.shiba, 0, np.sqrt(2) / 2 / np.pi))
+    # print(me.D(shib.shiba, 0, np.sqrt(2) / 2 / np.pi))
+    # print(me.cc(me.D(shib.shiba, 0, np.sqrt(20 / 2 / np.pi))))
+    #
+    # print(np.pi / 4 / shib.lat.phase(shib.shiba.B, np.array([1, 0]), np.array([0, 1])))

src/matrixElement.py
@@ -16,6 +16,7 @@ def A(shib: Shiba, phase: float, R: float) -> complex:
     matrix = -4 * alpha**2 / (1 + alpha**2) ** (3 / 2)
     matrix = matrix * exp(-R / xi) / 2 / np.pi / R
     matrix = matrix * cos(phase) * cos(2 * pi * R) * sin(delta)
+    print("A:", matrix)
     return matrix
 
 
@@ -34,6 +35,7 @@ def B(shib: Shiba, phase: float, R: float) -> complex:
         cos(phase) * cos(2 * pi * R) * sin(delta)
         + 1j * sin(phase) * sin(2 * pi * R) * cos(delta)
     )
+    print("B:", matrix)
     return matrix
 
 
@@ -50,6 +52,7 @@ def C(shib: Shiba, phase: float, R: float) -> complex:
         cos(phase) * sin(2 * pi * R) * cos(delta)
         + 1j * sin(phase) * cos(2 * pi * R) * sin(delta)
     )
+    print("C:", matrix)
     return matrix
 
 
@@ -67,6 +70,7 @@ def D(shib: Shiba, phase: float, R: float) -> complex:
         cos(phase) * sin(2 * pi * R) * cos(delta)
         + 1j * sin(phase) * cos(2 * pi * R) * sin(delta)
     )
+    print("D:", matrix)
     return matrix
 
 
@@ -212,8 +216,8 @@ def thirdOrder(
     # matrix2 = hop(shib, lat, coord3, coord1)
     # matrix3 = destroy(shib, lat, coord2, coord3)
     matrix1 = create(shib, lat, coord1, coord2)
-    matrix2 = hop(shib, lat, coord3, coord1)
-    matrix3 = destroy(shib, lat, coord2, coord3)
+    matrix2 = hop(shib, lat, coord2, coord3)
+    matrix3 = destroy(shib, lat, coord3, coord1)
     operator1 = matrix3 @ matrix2 @ matrix1
     operator2 = U() @ operator1 @ U().T
     operator3 = U() @ operator2 @ U().T