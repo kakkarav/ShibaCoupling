bccb3dd27baea49846164b19ff41733b4c4a7395
Kamphol Akkaravarawong <best@Kamphols-MacBook-Pro.local>
2024-04-19 22:23:08 -0700

    Add the new interactions

--- src/matrix_element_approx.py
+++ src/matrix_element_approx.py
@@ -2,224 +2,97 @@ import numpy as np
 from numpy import cos, sin, pi, exp
 from src.classes.params import Shiba
 from src.classes.lattice import Lattice
-from src.decomposition import decompose
 
 
-def get_A(shib: Shiba, phase: float, R: float) -> complex:
-    """
-    Matrix element for Ising pair creation (the initial state is taken to be the ket).
-    We set Delta (superconducting gap) to be 1.
-    """
+def normalized_spatial_part(shib: Shiba, R: float):
     alpha = shib.alpha
     xi = shib.xi
-    delta = shib.delta
-    matrix = -4 * alpha**2 / (1 + alpha**2) ** (3 / 2)
-    matrix = matrix * exp(-R / xi) / 2 / np.pi / R
-    matrix = matrix * cos(2 * pi * R) * sin(delta)
-    # print("A:", matrix)
-    return matrix
+    normalization = 4 * alpha**2 / (1 + alpha**2) ** (3 / 2)
+    decay = exp(-R / xi) / (2 * np.pi * R)
+    return normalization * decay
 
 
-def get_B(shib: Shiba, phase: float, R: float) -> complex:
-    """
-    Matrix element for spin flip pair creation
-    We set Delta (superconducting gap = 1)
-    """
-    alpha = shib.alpha
-    beta = shib.beta
-    xi = shib.xi
-    delta = shib.delta
-    matrix = 8 * beta * alpha**2 / (1 + alpha**2) ** (3 / 2)
-    matrix = matrix * exp(-R / xi) / 2 / np.pi / R
-    matrix = matrix * (
-        cos(2 * pi * R) * sin(delta) + 1j * sin(phase) * sin(2 * pi * R) * cos(delta)
+def second_order_zz(shib: Shiba, R: float):
+    normalization = normalized_spatial_part(shib, R)
+    jzz = (
+        -(normalization**2)
+        * (1 + 8 * (shib.beta**2))
+        / 2.0
+        * cos(2 * pi * R)
+        * (np.sin(shib.delta) ** 2)
     )
-    # print("B:", matrix)
-    return matrix
+    return jzz
 
 
-def get_C(shib: Shiba, phase: float, R: float) -> complex:
-    """
-    Matrix element for Ising hopping.
-    """
-    alpha = shib.alpha
-    xi = shib.xi
-    delta = shib.delta
-    matrix = -4 * alpha**2 / (1 + alpha**2) ** (3 / 2)
-    matrix = matrix * exp(-R / xi) / 2 / np.pi / R
-    matrix = matrix * (
-        sin(2 * pi * R) * cos(delta) + 1j * sin(phase) * cos(2 * pi * R) * sin(delta)
+def second_order_xx(shib: Shiba, R: float):
+    normalization = normalized_spatial_part(shib, R)
+    jxx = (
+        -(normalization**2)
+        * 4
+        * (shib.beta**2)
+        * cos(2 * pi * R)
+        * (np.sin(shib.delta) ** 2)
     )
-    # print("C:", matrix)
-    return matrix
+    return jxx
 
 
-def get_D(shib: Shiba, phase: float, R: float) -> complex:
-    """
-    Matrix element for spin-flip hopping.
-    """
-    alpha = shib.alpha
-    beta = shib.beta
-    xi = shib.xi
-    delta = shib.delta
-    matrix = 8 * beta * alpha**2 / (1 + alpha**2) ** (3 / 2)
-    matrix = matrix * exp(-R / xi) / 2 / np.pi / R
-    matrix = matrix * (
-        sin(2 * pi * R) * cos(delta) + 1j * sin(phase) * cos(2 * pi * R) * sin(delta)
+def third_order_zz_intermediate(shib: Shiba, R12: float, R23: float, R31: float):
+    n1 = normalized_spatial_part(shib, R12)
+    n2 = normalized_spatial_part(shib, R23)
+    n3 = normalized_spatial_part(shib, R31)
+    trig = (
+        -sin(2 * pi * R12) * cos(2 * pi * R23) * cos(2 * pi * R31)
+        + sin(2 * pi * R23) * cos(2 * pi * R31) * cos(2 * pi * R12)
+        + sin(2 * pi * R31) * cos(2 * pi * R12) * cos(2 * pi * R23)
     )
-    # print("D:", matrix)
-    return matrix
-
-
-def cc(number: complex) -> complex:
-    return np.conj(number)
-
-
-def hc(matrix: np.ndarray) -> np.ndarray:
-    return np.conj(matrix).T
-
-
-def destroy(
-    shiba: Shiba,
-    lat: Lattice,
-    coord1: np.ndarray,
-    coord2: np.ndarray,
-) -> np.ndarray:
-    """
-    Transfer matrix between subgap states from spin conserving terms
-    This matrix destroys quasiparticle at the first and second position of the Pauli string index (coord2, *, coord1).
-    """
-    R = lat.distance(coord1, coord2)
-    phase = lat.phase(shiba.B, coord1, coord2)
-    Aij = get_A(shiba, phase, R)
-    Bij = get_B(shiba, phase, R)
-    return np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [cc(Bij), Aij, 0, 0, 0, Bij, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, cc(Bij), Aij, 0, 0, 0, Bij],
-            [-Bij, 0, 0, 0, -Aij, -cc(Bij), 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, -Bij, 0, 0, 0, -Aij, -cc(Bij)],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-        ]
+    jzz = (
+        -(n1 * n2 * n3)
+        * (-1 + 4 * (shib.beta**2))
+        / 2.0
+        * trig
+        * cos(shib.delta)
+        * sin(shib.delta) ** 2
+        * (np.sin(shib.delta) ** 2)
+        * np.cos(shib.delta)
     )
+    return jzz
 
 
-def create(
-    shiba: Shiba,
-    lat: Lattice,
-    coord1: np.ndarray,
-    coord2: np.ndarray,
-) -> np.ndarray:
-    """
-    Transfer matrix between subgap states from spin-flipping terms.
-    This matrix create quasiparticle at the first and second positions of the Pauli string index (coord1, coord2, *)
-    """
-    R = lat.distance(coord1, coord2)
-    phase = lat.phase(shiba.B, coord1, coord2)
-    Aij = get_A(shiba, phase, R)
-    Bij = get_B(shiba, phase, R)
-    return hc(
-        np.array(
-            [
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [-Bij, 0, -Aij, 0, 0, 0, -cc(Bij), 0],
-                [0, -Bij, 0, -Aij, 0, 0, 0, -cc(Bij)],
-                [cc(Bij), 0, 0, 0, Aij, 0, Bij, 0],
-                [0, cc(Bij), 0, 0, 0, Aij, 0, Bij],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-            ]
-        )
+def third_order_xx(shib: Shiba, R12: float, R23: float, R31: float):
+    n1 = normalized_spatial_part(shib, R12)
+    n2 = normalized_spatial_part(shib, R23)
+    n3 = normalized_spatial_part(shib, R31)
+    trig = (
+        -sin(2 * pi * R12) * cos(2 * pi * R23) * cos(2 * pi * R31)
+        + sin(2 * pi * R23) * cos(2 * pi * R31) * cos(2 * pi * R12)
+        + sin(2 * pi * R31) * cos(2 * pi * R12) * cos(2 * pi * R23)
     )
-
-
-def hop(
-    shiba: Shiba,
-    lat: Lattice,
-    coord1: np.ndarray,
-    coord2: np.ndarray,
-) -> np.ndarray:
-    """
-    Transfer matrix between subgap states from pairing term.
-    The YSR quasiparticle hop from the second to the third index, i.e. (occupied, coord1, *) -> (occupied, *, coord2).
-    """
-    R = lat.distance(coord1, coord2)
-    phase = lat.phase(shiba.B, coord1, coord2)
-    Cij = get_C(shiba, phase, R)
-    Dij = get_D(shiba, phase, R)
-    return np.array(
-        [
-            [Cij, 0, Dij, 0, 0, 0, 0, 0],
-            [Dij, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, Dij, 0, 0, 0, 0],
-            [0, Dij, 0, Cij, 0, 0, 0, 0],
-            [0, 0, 0, 0, Cij, 0, Dij, 0],
-            [0, 0, 0, 0, Dij, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, Dij],
-            [0, 0, 0, 0, 0, Dij, 0, Cij],
-        ]
+    jxx = (
+        -(n1 * n2 * n3)
+        * 2.0
+        * (shib.beta**2)
+        * trig
+        * cos(shib.delta)
+        * sin(shib.delta) ** 2
+        * (np.sin(shib.delta) ** 2)
+        * np.cos(shib.delta)
     )
+    return jxx
 
 
-def U():
-    """
-    Permutation matrix that permute the indices cyclically.
-    """
-    return np.array(
-        [
-            [1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1],
-        ]
-    )
-
-
-def secondOrder(
-    lat: Lattice,
-    shib: Shiba,
-    coord1: np.ndarray,
-    coord2: np.ndarray,
-) -> dict[tuple, float]:
-    """
-    Return the table of all coupling arising from second order perturbation theory.
-    We use Pauli operator as our basis.
-    Spin at the third index does not participate in the interaction.
-    """
-    matrix = create(shib, lat, coord1, coord2)
-    return decompose(hc(matrix) @ matrix)
-
-
-def thirdOrder(
-    lat: Lattice,
-    shib: Shiba,
-    coord1: np.ndarray,
-    coord2: np.ndarray,
-    coord3: np.ndarray,
-) -> dict[tuple, float]:
-    """
-    Return the table of all coupling arising from third order perturbation theory.
-    We use Pauli string (i, j, k) which corresponds to position (coord1, coord2, coord3) as our basis.
-    """
-    matrix1 = create(shib, lat, coord1, coord2)
-    matrix2 = hop(shib, lat, coord2, coord3)
-    matrix3 = destroy(shib, lat, coord3, coord1)
-    operator1 = matrix3 @ matrix2 @ matrix1
-    operator2 = U() @ operator1 @ U().T
-    operator3 = U() @ operator2 @ U().T
-    return decompose(
-        operator1
-        + operator2
-        + operator3
-        + hc(operator1)
-        + hc(operator2)
-        + hc(operator3)
+def chiral_interaction(shib: Shiba, R12: float, R23: float, R31: float):
+    s = (R12 + R23 + R31) / 2.0
+    area = np.sqrt(s * (s - R12) * (s - R23) * (s - R31))
+    n1 = normalized_spatial_part(shib, R12)
+    n2 = normalized_spatial_part(shib, R23)
+    n3 = normalized_spatial_part(shib, R31)
+    return (
+        n1
+        * n2
+        * n3
+        * cos(area * shib.B)
+        * cos(shib.delta) ** 3
+        * cos(2 * np.pi * R12)
+        * cos(2 * np.pi * R23)
+        * cos(2 * np.pi * R31)
     )
